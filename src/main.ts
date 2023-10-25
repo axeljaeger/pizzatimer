3d7a31ef8596d84bb8331f82c2540be66a28fdea
Axel Jäger <axeljaeger@gmail.com>
2023-10-25 22:13:37 +0200

    Add PWA support.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 
-import { importProvidersFrom } from '@angular/core';
+import { importProvidersFrom, isDevMode } from '@angular/core';
 import { AppComponent } from './app/app.component';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatButtonModule } from '@angular/material/button';
@@ -9,12 +9,17 @@ import { MatIconModule } from '@angular/material/icon';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { provideServiceWorker } from '@angular/service-worker';
 
 
 bootstrapApplication(AppComponent, {
     providers: [
-        importProvidersFrom(BrowserModule, FormsModule, ReactiveFormsModule, MatIconModule, MatButtonModule, MatProgressSpinnerModule),
-        provideAnimations()
-    ]
+    importProvidersFrom(BrowserModule, FormsModule, ReactiveFormsModule, MatIconModule, MatButtonModule, MatProgressSpinnerModule),
+    provideAnimations(),
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })
+]
 })
   .catch(err => console.error(err));
